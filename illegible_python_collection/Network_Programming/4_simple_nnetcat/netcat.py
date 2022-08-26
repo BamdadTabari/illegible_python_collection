fe51fcc4c0b966a21ff5206623b0cec21bfd7b86
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2022-08-26 03:12:48 -0700

    fix a little mistake

--- illegible_python_collection/Network_Programming/4_simple_nnetcat/netcat.py
+++ illegible_python_collection/Network_Programming/4_simple_nnetcat/netcat.py
@@ -16,33 +16,6 @@ def execute(cmd):
     output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
     return output.decode()
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='BHP Net Tool',
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    epilog=textwrap.dedent('''Example:
-    netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
-    netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
-    netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
-    echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
-    netcat.py -t 192.168.1.108 -p 5555 # connect to server
-    '''))
-    parser.add_argument('-c', '--command', action='store_true', help='command shell')
-    parser.add_argument('-e', '--execute', help='execute specified command')
-    parser.add_argument('-l', '--listen', action='store_true', help='listen')
-    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
-    parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
-    parser.add_argument('-u', '--upload', help='upload file')
-
-    args = parser.parse_args()
-    if args.listen:
-        buffer = ''
-    else:
-       buffer = sys.stdin.read()
-    
-    nc = NetCat(args, buffer.encode())
-    nc.run()
-
 class NetCat:
     def __init__(self, args, buffer=None):
         self.args = args
@@ -126,3 +99,30 @@ class NetCat:
                     print(f'server killed {e}')
                     self.socket.close()
                     sys.exit()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='BHP Net Tool',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    epilog=textwrap.dedent('''Example:
+    netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
+    netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
+    netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
+    echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
+    netcat.py -t 192.168.1.108 -p 5555 # connect to server
+    '''))
+    parser.add_argument('-c', '--command', action='store_true', help='command shell')
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('-l', '--listen', action='store_true', help='listen')
+    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
+    parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
+    parser.add_argument('-u', '--upload', help='upload file')
+
+    args = parser.parse_args()
+    if args.listen:
+        buffer = ''
+    else:
+       buffer = sys.stdin.read()
+    
+    nc = NetCat(args, buffer.encode())
+    nc.run()
